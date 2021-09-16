181d4b805c481b20e95df7f120be3cf527b6d89c
joaopedro-mf <joaopedrom98@gmail.com>
2021-09-15 23:42:21 -0300

    First Commit

---
+++ src/Program.cs
@@ -0,0 +1,38 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace PassManager
+{
+    [Command( Description = "Pass Manager Application")]
+    [HelpOption]
+    [Subcommand(typeof(StartCommand)) ]
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await CreateHostBuilder().
+                RunCommandLineApplicationAsync<Program>(args) ;
+        }
+         
+
+        public static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder();
+        }
+
+
+        private void OnExecute(CommandLineApplication app, IConsole console)
+        {
+            var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+            Console.WriteLine($" ########## Pass Manager v{assemblyVersion} ##########");
+            Console.WriteLine($" *  To execute application, use in prompt  ./PassManager.exe start ");
+        }
+    }
+
+
+    
+}
---
+++ src/StartCommand.cs
@@ -0,0 +1,15 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System.Threading.Tasks;
+
+namespace PassManager
+{
+    [Command(Name = "start", Description = "Start Application")]
+    class StartCommand
+    {
+        private async Task OnExecute(IConsole console)
+        {
+            await Task.CompletedTask;
+            console.WriteLine($"Current Time");
+        }
+    }
+}
